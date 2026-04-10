e0d61ee2378a70645b91e1aca07281224188edb3
sanah sadique <sanah@Mac.home.local>
2026-04-10 21:01:20 +0100

    Update demo button label to 29 seconds

--- app/page.js
+++ app/page.js
@@ -258,7 +258,7 @@ export default function HomePage() {
                   marginLeft: 3,
                 }} />
               </div>
-              Watch 20-second demo
+              Watch 29-second demo
             </button>
           </div>
 
